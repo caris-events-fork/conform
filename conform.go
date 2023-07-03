144d07426afe81a00a21fe37c86694bc88a2d19c
Yami Odymel <yamiodymel@gmail.com>
2023-07-04 01:13:06 +0800

    Updated for strcase, camel->pascal, slug -> kebab

--- conform.go
+++ conform.go
@@ -11,7 +11,7 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/etgryphon/stringUp"
+	"github.com/iancoleman/strcase"
 )
 
 type x map[string]string
@@ -48,91 +48,6 @@ func emailDomainPart(s string) string {
 func email(s string) string {
 	// According to rfc5321, "The local-part of a mailbox MUST BE treated as case sensitive"
 	return emailLocalPart(s) + "@" + strings.ToLower(emailDomainPart(s))
-}
-
-func camelTo(s, sep string) string {
-	var result string
-	var words []string
-	var lastPos int
-	rs := []rune(s)
-
-	for i := 0; i < len(rs); i++ {
-		if i > 0 && unicode.IsUpper(rs[i]) {
-			if initialism := startsWithInitialism(s[lastPos:]); initialism != "" {
-				words = append(words, initialism)
-
-				i += len(initialism) - 1
-				lastPos = i
-				continue
-			}
-
-			words = append(words, s[lastPos:i])
-			lastPos = i
-		}
-	}
-
-	// append the last word
-	if s[lastPos:] != "" {
-		words = append(words, s[lastPos:])
-	}
-
-	for k, word := range words {
-		if k > 0 {
-			result += sep
-		}
-
-		result += strings.ToLower(word)
-	}
-
-	return result
-}
-
-// startsWithInitialism returns the initialism if the given string begins with it
-func startsWithInitialism(s string) string {
-	var initialism string
-	// the longest initialism is 5 char, the shortest 2
-	for i := 1; i <= 5; i++ {
-		if len(s) > i-1 && commonInitialisms[s[:i]] {
-			initialism = s[:i]
-		}
-	}
-	return initialism
-}
-
-// commonInitialisms, taken from
-// https://github.com/golang/lint/blob/3d26dc39376c307203d3a221bada26816b3073cf/lint.go#L482
-var commonInitialisms = map[string]bool{
-	"API":   true,
-	"ASCII": true,
-	"CPU":   true,
-	"CSS":   true,
-	"DNS":   true,
-	"EOF":   true,
-	"GUID":  true,
-	"HTML":  true,
-	"HTTP":  true,
-	"HTTPS": true,
-	"ID":    true,
-	"IP":    true,
-	"JSON":  true,
-	"LHS":   true,
-	"QPS":   true,
-	"RAM":   true,
-	"RHS":   true,
-	"RPC":   true,
-	"SLA":   true,
-	"SMTP":  true,
-	"SSH":   true,
-	"TLS":   true,
-	"TTL":   true,
-	"UI":    true,
-	"UID":   true,
-	"UUID":  true,
-	"URI":   true,
-	"URL":   true,
-	"UTF8":  true,
-	"VM":    true,
-	"XML":   true,
 }
 
 func ucFirst(s string) string {
@@ -325,12 +240,14 @@ func transformString(input, tags string) string {
 			input = strings.ToUpper(input)
 		case "title":
 			input = strings.Title(input)
+		case "pascal":
+			input = strcase.ToCamel(input)
 		case "camel":
-			input = stringUp.CamelCase(input)
+			input = strcase.ToLowerCamel(input)
 		case "snake":
-			input = camelTo(stringUp.CamelCase(input), "_")
-		case "slug":
-			input = camelTo(stringUp.CamelCase(input), "-")
+			input = strcase.ToSnake(input)
+		case "kebab":
+			input = strcase.ToKebab(input)
 		case "ucfirst":
 			input = ucFirst(input)
 		case "name":
